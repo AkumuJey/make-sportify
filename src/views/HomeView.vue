f89dac326922965508b002953f40403fc697c20d
AkumuJey <josephowino7@gmail.com>
2023-02-16 15:27:07 +0300

    Added the music selection components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,6 +16,44 @@ import HomeCard from '../components/HomeCard.vue'
     <div class="flex items-center"
     >
      <HomeCard image="https://picsum.photos/id/30/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard image="https://picsum.photos/id/44/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="md:block hidden" image="https://picsum.photos/id/58/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="lg:block hidden" image="https://picsum.photos/id/69/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="xl:block hidden" image="https://picsum.photos/id/70/300/300" title="Title is here" subTitle="Subtital is here"/>
+    </div>
+  </div>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Recommended Radio
+    </button>
+    <div class="py-1.5"></div>
+    <div class="flex items-center"
+    >
+     <HomeCard image="https://picsum.photos/id/23/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard image="https://picsum.photos/id/46/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="md:block hidden" image="https://picsum.photos/id/55/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="lg:block hidden" image="https://picsum.photos/id/66/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="xl:block hidden" image="https://picsum.photos/id/100/300/300" title="Title is here" subTitle="Subtital is here"/>
+    </div>
+  </div>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Recommended Songs
+    </button>
+    <div class="py-1.5"></div>
+    <div class="flex items-center"
+    >
+     <HomeCard image="https://picsum.photos/id/38/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard image="https://picsum.photos/id/50/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="md:block hidden" image="https://picsum.photos/id/63/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="lg:block hidden" image="https://picsum.photos/id/72/300/300" title="Title is here" subTitle="Subtital is here"/>
+     <HomeCard class="xl:block hidden" image="https://picsum.photos/id/80/300/300" title="Title is here" subTitle="Subtital is here"/>
     </div>
   </div>
 </template>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import CategorySelect from '../components/CategorySelect.vue'
+</script>
+<template>
+  <div>
+      <div class="p-8">
+        <button
+          type="button"
+          class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+          Recommended Songs
+        </button>
+        <div class="py-1.5"></div>
+        <div class="grid xl:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 gap-6"
+        >
+         <CategorySelect category="Podscast" image="https://picsum.photos/id/21/300/300"/>
+         <CategorySelect category="Audio Book" image="https://picsum.photos/id/11/300/300"/>
+         <CategorySelect category="Surgery Talk" image="https://picsum.photos/id/15/300/300"/>
+         <CategorySelect category="Hip Hop" image="https://picsum.photos/id/17/300/300"/>
+         <CategorySelect category="Rhumba Nights" image="https://picsum.photos/id/19/300/300"/>
+         <CategorySelect category="Ohangla" image="https://picsum.photos/id/25/300/300"/>
+         <CategorySelect category="Benga" image="https://picsum.photos/id/75/300/300"/>
+         <CategorySelect category="RNBS" image="https://picsum.photos/id/95/300/300"/>
+         <CategorySelect category="Football Punditry" image="https://picsum.photos/id/9/300/300"/>
+         <CategorySelect category="Worship" image="https://picsum.photos/id/5/300/300"/>
+         <CategorySelect category="Soul" image="https://picsum.photos/id/2/300/300"/>
+         <CategorySelect category="Gengetone" image="https://picsum.photos/id/1/300/300"/>
+         <CategorySelect category="Dudu" image="https://picsum.photos/id/13/300/300"/>
+         <CategorySelect category="Ngolo" image="https://picsum.photos/id/91/300/300"/>
+         <CategorySelect category="Taarab" image="https://picsum.photos/id/93/300/300"/>
+         <CategorySelect category="Bongo" image="https://picsum.photos/id/99/300/300"/>
+        </div>
+      </div>
+  </div>
+</template>
+
